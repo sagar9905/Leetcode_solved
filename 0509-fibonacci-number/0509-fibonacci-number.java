a04848a433e0fb94dc213396b6efc8a4a62d0dae
sagar9905 <12sagardon@gmail.com>
2026-02-24 11:41:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (31.34%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+       int dp[] =new int[n+1]; 
+    
+       for(int i=0; i<=n;i++){
+        if(i<=1) dp[i]=i;
+        else{
+        dp[i]= dp[i-1]+dp[i-2];
+       }
+       }
+       return dp[n];
+    }
+}
