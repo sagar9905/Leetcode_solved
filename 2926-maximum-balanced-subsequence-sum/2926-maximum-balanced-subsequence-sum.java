ac99aa454e8393852b15725439d71f4a3cc097f1
sagar9905 <12sagardon@gmail.com>
2026-02-28 02:55:29 +0530

    Time: 323 ms (17.95%), Space: 94.3 MB (57.69%) - LeetHub

---
+++ 2926-maximum-balanced-subsequence-sum/2926-maximum-balanced-subsequence-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long maxBalancedSubsequenceSum(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = nums[i]-i;
+        }
+        TreeMap<Integer, Long> map = new TreeMap<>();
+        long ans = Integer.MIN_VALUE;
+        for(int i = 0; i<n; i++){
+            if(nums[i]<=0){
+                ans = Math.max(ans, nums[i]);
+            }
+            else{
+                long temp = nums[i];
+                if(map.floorKey(arr[i])!=null){
+                    temp += map.get(map.floorKey(arr[i]));
+                }
+                while(map.ceilingKey(arr[i])!=null && map.get(map.ceilingKey(arr[i]))<temp){
+                    map.remove(map.ceilingKey(arr[i]));
+                }
+                if(map.floorKey(arr[i])==null || map.get(map.floorKey(arr[i]))<temp){
+                    map.put(arr[i], temp);
+                }
+                ans = Math.max(ans, temp);
+            }
+        }
+        return ans;
+    }
+}
