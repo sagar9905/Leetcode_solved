eb2d062c9802c9dae859fbb521b7f90973066e37
sagar9905 <12sagardon@gmail.com>
2026-05-27 18:28:13 +0530

    Time: 60 ms (0.57%), Space: 42.9 MB (18.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -6,7 +6,7 @@ class Solution {
         return nums[0];
     }
     dp[0]=nums[0];
-    dp[1]=Math.max(nums[0],nums[1]);
+    dp[1]=Math.max(nums[1],nums[0]);
     for(int i=2; i<nums.length; i++){
         dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
     }
