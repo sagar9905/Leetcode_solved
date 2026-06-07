e702ade33a8cf5b047801abd4b3a051eeeffd4d0
sagar9905 <12sagardon@gmail.com>
2026-06-07 16:38:01 +0530

    Time: 30 ms (29.13%), Space: 47.4 MB (15.23%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -5,14 +5,16 @@ class Solution {
       Arrays.fill(dp,1);
       int maxlen=0;
        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
-      for(int i=0; i<n;i++){
-        for(int j=0; j<i;j++){
-            if(pairs[j][1]<pairs[i][0]){
-                dp[i] =Math.max(dp[i],dp[j]+1);
+       for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            if(pairs[j][1]< pairs[i][0]){
+                dp[i]= Math.max(dp[i], 1+dp[j]);
             }
         }
-        maxlen  =Math.max(maxlen, dp[i]);
-      }
-        return maxlen; 
-       }  
+       }
+       for(int i=0; i<n; i++){
+        maxlen = Math.max(maxlen, dp[i]);
+       } 
+       return maxlen;
     }
+}
